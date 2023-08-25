f001c290a13df3701cc7fe0b81f3a8ac55bed0a0
Amara2019 <emmanuelamarachi55@gmail.com>
2023-08-25 12:43:11 +0100

    done with header

--- public/index.php
+++ public/index.php
@@ -5,16 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AirBnB</title>
     <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="fa_icons/css/all.min.css">    <!-- <link rel="stylesheet" href=""> -->
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">    <link rel="stylesheet" href=""> -->
+
 </head>
 <body>
+    <?php 
+        include_once "components/header.html";
+    ?>
 
-    <h1 class="text-red-500 text-7xl bg-color-light">
-        Hello World!
-    </h1>
 
-    <div class="airbnb-card">
-        AirBnB
-    </div>
+
     
+    
+    <?php 
+        include_once "components/footer.html";
+    ?>
 </body>
 </html>
